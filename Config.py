8c27670bc23f99a8e3ce15d17d709664573ec86d
Muhammedhafis <129869634+Muhammedhafis@users.noreply.github.com>
2025-11-02 11:47:04 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 API_ID = "<PUT API-ID HERE"
 API_HASH = "<PUT API-HASH HERE>"
-BOT_TOKEN = "<PUT BOT-TOKEN HERE"
+BOT_TOKEN = "7245981928:AAGzDckedeyn8cOPdJ8X0-7ojiBsyFnmKAk"
 API_KEY = "<PUT API-KEY HERE>" # API-KEY gets from https://api.xteam.xyz/
